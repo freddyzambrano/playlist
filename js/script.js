ec39cc2b0f186fdc7a52d000c24f95fab105aea0
Freddy Zambrano <fr3batty@icloud.com>
2019-02-04 23:16:47 +0000

    day1

--- js/script.js
+++ js/script.js
@@ -1,7 +1,36 @@
 /* global $ */
 
 // BELOW Update the songs array with four of your favorites songs.
-var songs = [];
+var songs = [
+    "Respect", 
+    "Girl on Fire", 
+    "The List", 
+    "Take on Me"
+];
+var images_links = [
+    "/images/respect", 
+    "/images/girl-on-fire.jpeg", 
+    "/images/the-list.jpeg", 
+    "/images/take-on-me.jpeg"
+];
+var artists = [
+    "Aretha Franklin", 
+    "Alicia Keys", 
+    "Moonchild", 
+    "A-ha"
+];
+var songLengths = [
+    "2:30", 
+    "3:52", 
+    "5:10", 
+    "3:47"
+];
+var links = [
+    "https://www.youtube.com/watch?v=6FOUqQt3Kg0", 
+    "https://www.youtube.com/watch?v=J91ti_MpdHA", 
+    "https://www.youtube.com/watch?v=1eDvtvdoxJE", 
+    "https://www.youtube.com/watch?v=djV11Xbc914"
+];
 // BELOW Add 4 More arrays to store images_links, artists, song lengths, and links for each song
 // Make sure they match the same order as your array above
 
